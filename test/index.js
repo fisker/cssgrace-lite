7140c7f15a8ca5a74d46f60e68e70eb686ad949a
fisker <lionkay@gmail.com>
2018-06-19 18:29:52 +0800

    init

--- test/index.js
+++ test/index.js
@@ -15,7 +15,7 @@ function compareFixtures(t, name, msg, opts, postcssOpts) {
   var actual = postcss().use(plugin).process(read(postcssOpts.from), postcssOpts).css
 
   var expected = read(filename('fixtures/' + name + '-out'))
-  fs.writeFile(filename('fixtures/' + name + '-real'), actual)
+  fs.writeFileSync(filename('fixtures/' + name + '-real'), actual)
   t.equal(actual.trim(), expected.trim(), msg)
 }
 
@@ -26,26 +26,6 @@ test('remove display', function(t) {
 
 test('remove colons', function(t) {
   compareFixtures(t, 'remove-colons', 'Should be remove colons')
-  t.end()
-})
-
-test('position center mixin', function(t) {
-  compareFixtures(t, 'position-center', 'Should be transform')
-  t.end()
-})
-
-test('ellipsis mixin', function(t) {
-  compareFixtures(t, 'ellipsis', 'Should be transform')
-  t.end()
-})
-
-test('resize mixin', function(t) {
-  compareFixtures(t, 'resize', 'Should be transform')
-  t.end()
-})
-
-test('clearfix mixin', function(t) {
-  compareFixtures(t, 'clearfix', 'Should be transform')
   t.end()
 })
 
@@ -64,34 +44,7 @@ test('inline-block hack', function(t) {
   t.end()
 })
 
-test('image set mixin', function(t) {
-  compareFixtures(t, 'image-set', 'Should be transform')
-  t.end()
-})
-
-test('image size', function(t) {
-  compareFixtures(t, 'image-size', 'Should be transform')
-  t.end()
-})
-
 test('comment', function(t) {
   compareFixtures(t, 'comment', 'Should be keep the comments on the current line')
   t.end()
 })
-
-test('image-size-multiple', function(t) {
-  compareFixtures(t, 'image-size-multiple', 'Should be transform')
-  t.end()
-})
-
-test('base64-image-size', function (t) {
-  compareFixtures(t, 'base64-image-size', 'Should be transform')
-  t.end();
-});
-
-test('hash', function (t) {
-  compareFixtures(t, 'hash', 'Should be remove hash')
-  t.end();
-})
-
-
